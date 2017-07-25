cf4ed5165abce7b5572db107a1a4948f77e3cd95
Raphael Amorim <rapha850@gmail.com>
2017-07-25 18:19:47 -0300

    add benchmark tests

---
+++ benchmarks/ofs.js
@@ -0,0 +1,20 @@
+const http = require('http')
+const port = 3000
+const main = require('./src/main')
+
+const requestHandler = (request, response) => {
+  console.log('requested')
+  main.read('./test/fixtures/2mb.txt').on('end', (data) => {
+    response.end(data.content)
+  })
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
